53e865bbf10088ef2daf9c028da40ccd4c472660
Harisfazillah Jamel <linuxmalaysia@gmail.com>
2015-06-25 06:09:08 +0800

    Debugging server issues with new version of Apache and PHP

--- get.php
+++ get.php
@@ -11,8 +11,18 @@ $opts = array(
 
 $context = stream_context_create($opts);
 
+// Debuging
+
+$jsondata =file_get_contents($_GET["url"],false,$context);
+echo $jsondata;
+
+//
+
 $prefix = substr($_GET["url"], 0, 4);
 if($prefix != "http") die();
 echo file_get_contents($_GET["url"],false,$context);
+
+
+
 //header("Location: ".$_GET["url"]);
 ?>
