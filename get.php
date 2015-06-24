701588843c7a8d2366bdee310c8984bce8ea5113
Harisfazillah Jamel <linuxmalaysia@gmail.com>
2015-06-25 05:45:24 +0800

    Refering to http://php.net/manual/en/function.file-get-contents.php

--- get.php
+++ get.php
@@ -1,6 +1,18 @@
 <?
+
+// Create a stream
+$opts = array(
+  'http'=>array(
+    'method'=>"GET",
+    'header'=>"Accept-language: en\r\n" .
+              "Cookie: foo=bar\r\n"
+  )
+);
+
+$context = stream_context_create($opts);
+
 $prefix = substr($_GET["url"], 0, 4);
 if($prefix != "http") die();
-//echo file_get_contents($_GET["url"]);
+echo file_get_contents($_GET["url"],false,$context);
 //header("Location: ".$_GET["url"]);
 ?>
